1b75ab76aba90134f7060b83bb354769d86f4a07
Кирилл Бурдов <Tyuiu.BurdovKS@outlook.com>
2024-11-25 19:12:43 +0300

    Task3

---
+++ Tyuiu.BurdovKS.Sprint5.Task3.V22.Lib/DataService.cs
@@ -0,0 +1,36 @@
+﻿
+
+
+
+using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+
+namespace Tyuiu.BurdovKS.Sprint5.Task3.V22.Lib
+{
+    public class DataService : ISprint5Task3V22
+    {
+        public string SaveToFileTextData(int x)
+        {
+
+
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+
+
+            double z;
+            double y;
+
+            z = (Math.Pow(1 - x, 2))/(-3 * x);
+
+            y = Math.Round(z, 3);
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(y));
+            }
+            return path;
+
+
+        }
+    }
+}
---
+++ Tyuiu.BurdovKS.Sprint5.Task3.V22/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+
+
+using Tyuiu.BurdovKS.Sprint5.Task3.V22.Lib;
+
+
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+
+        Console.Title = "Спринт #5 | Выполнил: Бурдов.К.С | СМАРТБ-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #22                                                             *");
+        Console.WriteLine("* Выполнил: Бурдов Кирилл Сергеевич |  СМАРТБ-24-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x = 3;
+        Console.WriteLine("x = " + x);
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+        Console.WriteLine("**************************************************************************");
+        string res = ds.SaveToFileTextData(x);
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан!");
+        Console.ReadKey();
+    }
+}
+
+
+
+
