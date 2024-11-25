71143bd62e8b01755caa8fa1038afd1b42763664
Кирилл Бурдов <Tyuiu.BurdovKS@outlook.com>
2024-11-25 23:19:15 +0300

    Task4

--- Tyuiu.BurdovKS.Sprint5.Task4.V29.Lib/DataService.cs
+++ Tyuiu.BurdovKS.Sprint5.Task4.V29.Lib/DataService.cs
@@ -14,29 +14,33 @@ namespace Tyuiu.BurdovKS.Sprint5.Task4.V29.Lib
 
             string strX = File.ReadAllText(path).Trim();
 
-            
             if (string.IsNullOrWhiteSpace(strX))
             {
                 throw new FormatException("Файл пуст или не содержит данных.");
             }
 
+            double y;
             double x;
-            
+
             if (!double.TryParse(strX, NumberStyles.Any, CultureInfo.InvariantCulture, out x))
             {
                 throw new FormatException($"Строка '{strX}' не содержит корректное числовое значение.");
             }
 
+            double temp = Convert.ToDouble(x);
 
 
-            double result = Math.Round((x / 2 * x) + Math.Pow(Math.Sin(x), 2), 3);
+            if (temp == 0) y = -1;
+            else y = Math.Round((temp / (2 * temp) + Math.Sin(Math.Pow(temp, 2))), 3);
 
-            
-            return result;
+            return y;
 
 
 
-           
+
+
+
+
 
         }
     }
