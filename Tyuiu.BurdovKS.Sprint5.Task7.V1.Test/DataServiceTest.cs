72f7dddf74538461e51cde87dfdd49f3704ff3c8
Кирилл Бурдов <Tyuiu.BurdovKS@outlook.com>
2024-11-26 11:29:39 +0300

    Task7

---
+++ Tyuiu.BurdovKS.Sprint5.Task7.V1.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+
+
+
+
+
+using Tyuiu.BurdovKS.Sprint5.Task7.V1.Lib;
+
+
+namespace Tyuiu.BurdovKS.Sprint5.Task7.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "InPutDataFileTask7V1.txt" });
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+
+
+
+
+        }
+    }
+}
---
+++ Tyuiu.BurdovKS.Sprint5.Task7.V1/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+
+
+using Tyuiu.BurdovKS.Sprint5.Task7.V1.Lib;
+
+using System.IO;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+
+        Console.Title = "Спринт #5 | Выполнил: Бурдов.К.С | СМАРТБ-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #1                                                              *");
+        Console.WriteLine("* Выполнил: Бурдов Кирилл Сергеевич |  СМАРТБ-24-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        DataService ds = new DataService();
+        string path = Path.Combine(new string[] { Path.GetTempPath(), "InPutDataFileTask7V1.txt" });
+
+
+
+
+
+        Console.WriteLine("данные находятся в файле " + path);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        string res = ds.LoadDataAndSave(path);
+        Console.WriteLine(res);
+
+    }
+}
